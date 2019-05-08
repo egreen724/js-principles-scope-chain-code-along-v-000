43a800827f10bb4819c1e7475b692361a63936e2
Erin Green <erin.y.green@gmail.com>
2019-05-08 14:43:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,10 +43,6 @@ function overWriteBestCustomer(newFavorite){
   bestCustomer = newFavorite
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
     let favoriteCustomer = 'not bar'
