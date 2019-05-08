0270f232f2291f7f8ae990fd44360bb2a7f5287e
Erin Green <erin.y.green@gmail.com>
2019-05-08 14:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
-// var customerName = 'bob';
-// const leastFavoriteCustomer = "Erin";
-//
+var customerName = 'bob';
+const leastFavoriteCustomer = "Erin";
+
 // function upperCaseCustomerName(){
 //    return customerName = customerName.toUppercase();
 // }
-//
+// 
 // function setBestCustomer(){
 //    bestCustomer = "not bob";
 // }
-//
+// 
 // function overWriteBestCustomer(newFavorite){
 //    bestCustomer = newFavorite;
 // }
-//
+// 
 // function changeLeastFavoriteCustomer(){
 //   leastFavoriteCustomer = "bob";
 // }
-//
+// 
 // function attemptTwofavoriteCustomers(){
 //   let favoriteCustomer = "bob"
-//
+// 
 //   let favoriteCustomer = "not bob"
 // }
-
-var customerName = 'bob';
-const leastFavoriteCustomer = 'sour fred';
-
-function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
-}
-
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
-}
-
-
-function setBestCustomer(){
-  bestCustomer = 'not bob'
-}
-
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
-}
-
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
-}
