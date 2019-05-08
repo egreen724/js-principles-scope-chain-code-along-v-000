430af0b30279f3a7060cad1767f3d5ef4415909f
Erin Green <erin.y.green@gmail.com>
2019-05-08 14:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var customerName = 'bob';
 const leastFavoriteCustomer = "Erin";
 
-// function upperCaseCustomerName(){
-//    return customerName = customerName.toUppercase();
-// }
-// 
+function upperCaseCustomerName(){
+   return customerName = customerName.toUppercase();
+}
+
 // function setBestCustomer(){
 //    bestCustomer = "not bob";
 // }
-// 
+//
 // function overWriteBestCustomer(newFavorite){
 //    bestCustomer = newFavorite;
 // }
-// 
+//
 // function changeLeastFavoriteCustomer(){
 //   leastFavoriteCustomer = "bob";
 // }
-// 
+//
 // function attemptTwofavoriteCustomers(){
 //   let favoriteCustomer = "bob"
-// 
+//
 //   let favoriteCustomer = "not bob"
 // }
